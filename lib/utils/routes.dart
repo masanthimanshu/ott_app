f9cf671580b54a991cd1a6b80240faefa18b673d
himanshumasant <himanshu.masant@wisepromo.com>
2024-02-03 11:26:45 +0530

    updates

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:ott_app/utils/root.dart';
 
-import '/view/auth/repository/auth_repository.dart';
+import '/view/auth/login/login_screen.dart';
+import '/view/auth/signup/signup_screen.dart';
 import '/view/get_started/get_started_screen.dart';
 import '/view/navigation/navigation_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   "/": (e) => const CustomRoot(),
-  "/auth": (e) => const AuthRepository(),
+  "/login": (e) => const LoginScreen(),
+  "/signup": (e) => const SignupScreen(),
   "/home": (e) => const NavigationScreen(),
   "/get-started": (e) => const GetStartedScreen(),
 };
